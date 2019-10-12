493f86a7a43e7008dfb6044138668d5942fde9b0
Graham <graham.gettel@gmail.com>
2019-10-12 19:32:21 -0400

    Added UI w/ OnChangedValue for DiscBehaviours
    UI Officially connected to the Flight Number variables. Playable, some bugs left

--- Disc Golf Demo/Assets/Scripts/DiscBehaviors.cs
+++ Disc Golf Demo/Assets/Scripts/DiscBehaviors.cs
@@ -7,11 +7,12 @@ public class DiscBehaviors : MonoBehaviour
     //disc Flight Numbers
     public float speed;
     public float adjustedSpeed;
-    public int glide;
-    public int adjustedGlide;
+    public float glide;
+    public float adjustedGlide;
     //turnFade is unique (two different Flight Numbers, but simply opposites); turn heads right (positive along axis), fade heads left (negative along axis)
-    public int turnFade;
-    public int adjustedTurnFade;
+    //for the UI turn and fade will actually manipulate the same value: turnFade (but they will seem different to the user visually)
+    public float turnFade;
+    public float adjustedTurnFade;
 
     //array and list for Speed modification
     public GameObject[] handAnchors;
@@ -21,15 +22,28 @@ public class DiscBehaviors : MonoBehaviour
     public GameObject gravityAlterationObject;
     public GravityAlteration gravityAlterationScript;
 
+    //get ThrowForce script for Turn & Fade
+    public GameObject throwDisc;
+    public ThrowForce throwForce;
+
+    //get the slider for behavior mods via UI - IF nothing else works
+    public GameObject slider;
+
     // Start is called before the first frame update
     void Start()
     {
+
+
         //get resources-
+        //find all objects that have the OVRGrabber script
+        handAnchors = GameObject.FindGameObjectsWithTag("HandAnchor");
+        
         //find gravity alteration
         gravityAlterationScript = gravityAlterationObject.GetComponent<GravityAlteration>();
 
-        //find all objects that have the OVRGrabber script
-        handAnchors = GameObject.FindGameObjectsWithTag("HandAnchor");
+        //find throwDisc & throw force
+        throwDisc = GameObject.FindGameObjectWithTag("Disc");
+        throwForce = throwDisc.GetComponent<ThrowForce>();
 
         //fill list with scripts, allows for us not to use GetComponent in Update(which is expensive)
         foreach (GameObject handAnchor in handAnchors)
@@ -52,7 +66,7 @@ public class DiscBehaviors : MonoBehaviour
                 for (int i = 0; i < handAnchors.Length; i++)
                 {
                     
-                    ovrGrabbers[i].throwMultiplier = speed / 7;
+                    ovrGrabbers[i].throwMultiplier = adjustedSpeed / 4;
                 }
                 speed = adjustedSpeed;
         }
@@ -71,5 +85,32 @@ public class DiscBehaviors : MonoBehaviour
             glide = adjustedGlide;
         }
 
+        //Update Fade/Turn if changed
+        if (turnFade != adjustedTurnFade)
+        {
+            throwForce.sideThrust = adjustedTurnFade;
+
+            turnFade = adjustedTurnFade;
+        }
+
+    }
+
+    //HOW TO ELIMIATE BELOW REPETITION - C# Delegate?
+
+    //so that the Slider can find this script and adjust the value
+    public void OnSliderValueChangedSpeed(float value)
+    {
+        adjustedSpeed = value;
+    }
+
+    //specifically for glide
+    public void OnSliderValueChangedGlide(float value)
+    {
+        adjustedGlide = value;
+    }
+
+    public void OnSliderValueChangedTurnFade(float value)
+    {
+        adjustedTurnFade = value;
     }
 }
--- Disc Golf Demo/Assets/Scripts/ThrowForce.cs
+++ Disc Golf Demo/Assets/Scripts/ThrowForce.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class ThrowForce : MonoBehaviour
 {
 
-    public float thrust;
-    public float descentThrust;
+    //public float thrust;
+    //public float descentThrust;
     public float sideThrust;
-    public Rigidbody rb;
+    public Rigidbody discRb;
     public ScoreKeeper scoreKeeper;
     //public HapticVibration hapticVibration;
     public DiscRespawn discRespawn;
     public bool addForce;
-    public float overTimeSlow;
+    public float overTimeSpeed;
+
+    //get factor of turn or fade
+    public int turnOrFade;
 
     public EventManager eventManager;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        discRb = GetComponent<Rigidbody>();
     }
 
     private void Update()
@@ -31,56 +34,85 @@ public class ThrowForce : MonoBehaviour
        else if (discRespawn.discLanded == true)
         {
             addForce = false;
+            overTimeSpeed = 0;
         }
     }
 
     void FixedUpdate()
     {
-        if (addForce == true)
+        if (addForce)
         {
-            //if disclanded is not true
-            //Debug.Log("the rigidbody velocity is: " + rb.velocity);
-            if (eventManager.discThrown.name != "PutterDisc")
+            //if side thrust had not reached 0 continue to decrease the thrust
+            if(sideThrust > 0)
             {
-                //makes the initial speed thrown for the user seem more realistic (instead of "slow mo")
-                if (scoreKeeper.score <= 60)
-                {
-                    //Debug.Log("thrust added");
-                    rb = eventManager.discThrown.GetComponent<Rigidbody>();
-                    overTimeSlow = overTimeSlow + Time.deltaTime;
-                    //thrust = thrust - overTimeSlow;
-
-                    //if right hand
-                    if (eventManager.rightHand == true)
-                    {
-                        rb.AddRelativeForce(transform.forward * -thrust); //*eventManager.initialDiscVelocity)
-                    }
-
-
-                    //if left hand
-                    if (eventManager.rightHand == false)
-                    {
-                        rb.AddRelativeForce(transform.forward * thrust); //*eventManager.initialDiscVelocity)
-                    }
-
-                }
+                overTimeSpeed = overTimeSpeed + Time.deltaTime;
             }
 
-            if ( scoreKeeper.score >= 50) //rb.velocity.x >= -6.0f
+            //right hand throw
+            if (eventManager.rightHand == true)
             {
-                //rb = eventManager.discThrown.GetComponent<Rigidbody>();
-
-
-                //alternative - consistent curve from throw start
-                //Vector3 sideDir = Vector3.Cross(transform.up, rb.velocity).normalized;
-               //rb.AddRelativeForce(0, 0, sideDir * curveAmount);
-
-
-                rb.AddRelativeForce(0,0,sideThrust);
-                rb.AddForce(0, -descentThrust, 0);
-                // Debug.Log("Sidethrust added");
+                turnOrFade = 1;
             }
+            else
+            {
+                turnOrFade = -1;
+            }
+       
+            //turn or fade
+            discRb.AddRelativeForce(0, 0, (sideThrust + (overTimeSpeed/2)) * turnOrFade);
+
         }
+        
+
+
+
+        //DELETE ALL THIS WHEN SET:
+
+        //if (addForce == true)
+        //{
+        //    //if disclanded is not true
+        //    //Debug.Log("the rigidbody velocity is: " + rb.velocity);
+        //    if (eventManager.discThrown.name != "PutterDisc")
+        //    {
+        //        //makes the initial speed thrown for the user seem more realistic (instead of "slow mo")
+        //        if (scoreKeeper.score <= 60)
+        //        {
+        //            //Debug.Log("thrust added");
+        //            //rb = eventManager.discThrown.GetComponent<Rigidbody>();
+        //            overTimeSpeed = overTimeSpeed + Time.deltaTime;
+        //            //thrust = thrust - overTimeSpeed;
+
+        //            //if right hand
+        //            if (eventManager.rightHand == true)
+        //            {
+        //                rb.AddRelativeForce(transform.forward * -thrust); //*eventManager.initialDiscVelocity)
+        //            }
+
+
+        //            //if left hand
+        //            if (eventManager.rightHand == false)
+        //            {
+        //                rb.AddRelativeForce(transform.forward * thrust); //*eventManager.initialDiscVelocity)
+        //            }
+
+        //        }
+        //    }
+
+        //    if ( scoreKeeper.score >= 50) //rb.velocity.x >= -6.0f
+        //    {
+        //        //rb = eventManager.discThrown.GetComponent<Rigidbody>();
+
+
+        //        //alternative - consistent curve from throw start
+        //        //Vector3 sideDir = Vector3.Cross(transform.up, rb.velocity).normalized;
+        //       //rb.AddRelativeForce(0, 0, sideDir * curveAmount);
+
+
+        //        rb.AddRelativeForce(0,0,sideThrust);
+        //        //rb.AddForce(0, -descentThrust, 0);
+        //        // Debug.Log("Sidethrust added");
+        //    }
+        //}
 
     }
 }
