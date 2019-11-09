76d51e3211c6a8794ed8294e299684fc11e704ee
Graham <graham.gettel@gmail.com>
2019-11-09 15:09:53 -0500

    Updated Script & Cleanup

--- Disc Golf Demo/Assets/Oculus/VR/Scripts/Util/OVRGrabber.cs
+++ Disc Golf Demo/Assets/Oculus/VR/Scripts/Util/OVRGrabber.cs
@@ -156,7 +156,7 @@ public class OVRGrabber : MonoBehaviour
 
 		float prevFlex = m_prevFlex;
         // Update values from inputs
-        m_prevFlex = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, m_controller); //changed from "Hand" to "Index" - 8.15
+        m_prevFlex = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, m_controller); //changed from "Hand" to "Index" - 8.15, changed back to Hand 11.7, revert soon.
 
         CheckForGrabOrRelease(prevFlex);
     }
